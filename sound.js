92bd21c6dc6f246e8128fbc2e3e4f8e2f35ef74c
harveynuel3011-sudo <harveynuel3011@gmail.com>
2025-11-20 15:28:23 +0800

    Update sound.js

--- sound.js
+++ sound.js
@@ -1,7 +1,7 @@
-
 const introSounds = [
-  new Audio('audio/what fuckin morron designed this (1).mp3'),
-  new Audio('audio/hi there.mp3')
+  new Audio('https://raw.githubusercontent.com/harveynuel3011/Going-postal/main/audio/what%20fuckin%20morron%20designed%20this%20(1).mp3'),
+  new Audio('https://raw.githubusercontent.com/harveynuel3011/Going-postal/main/audio/hi%20there.mp3')
+];
 ];
 
 introSounds.forEach(sound => {
